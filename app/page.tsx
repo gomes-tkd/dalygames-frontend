7137cb723a4e6fdf57782cc0f06a8d8680c64755
Jose Gomes <jgomestkd@gmail.com>
2025-11-02 23:13:27 -0300

    Finishing dalygame banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,21 @@ import Container from "@/components/container";
 import GameProps from "@/utils/types/game";
 import Link from "next/link";
 import Image from "next/image";
+import { BsArrowRightSquare } from "react-icons/bs";
 
 async function getDalyGame() {
     try {
         const response = await fetch(
-            `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+            `${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 }}
         );
 
         return response.json();
     } catch (err: unknown) {
-        if (err instanceof Error) {
-            console.error("Error fetching data:", err.message);
-        }
+        const errMessage = err instanceof Error ? err.message : String(err);
 
-        throw new Error("Failed to fetch data. Error: " + err.message);
+        console.error("Error fetching data: ", errMessage);
+
+        throw new Error("Failed to fetch data. Error: " + errMessage);
     }
 }
 
@@ -30,15 +31,32 @@ export default async function Home() {
                     Separamos um jogo exclusivo para ti.
                 </h1>
                 <Link href={`/game/${dalyGame.id}`}>
-                    <section className="w-full bg-black rounded-lg">
-                        <Image
-                            src={dalyGame.image_url}
-                            alt={dalyGame.title}
-                            priority={true}
-                            quality={100}
-                            width={100}
-                            height={100}
-                        />
+                    <section className={"w-full bg-black rounded-lg"}>
+                        <div className={"w-full max-h-96 h-96 relative rounded-lg"}>
+                            <div
+                                className={
+                                    "absolute z-20 bottom-0 p-3 justify-center items-center gap-2"
+                                }
+                            >
+                                <p className={"font-bold text-xl text-white"}>{dalyGame.title}</p>
+                                <BsArrowRightSquare
+                                    size={24}
+                                    color={"#fff"}
+                                />
+                            </div>
+                            <Image
+                                className={
+                                    "max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100"
+                                    + " transition-all duration-300"
+                                }
+                                src={dalyGame.image_url}
+                                alt={dalyGame.title}
+                                priority={true}
+                                quality={100}
+                                fill={true}
+                                sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 50vw"}
+                            />
+                        </div>
                     </section>
                 </Link>
             </Container>
